bb29d846e0530643aa809bf4370ee2b983005ad4
sue445 <sue445@sue445.net>
2023-11-23 09:26:59 +0900

    Remove unused node vars

--- .github/workflows/build_ruby.sh
+++ .github/workflows/build_ruby.sh
@@ -2,18 +2,11 @@
 
 readonly NODE_FILE="/tmp/node.yml"
 readonly BUNDLE=$(which bundle)
-readonly RUST_VERSION="1.58.1"
 
 cat << YAML > $NODE_FILE
 ruby:
   version: ${XBUILD_RUBY_VERSION}
   enabled_yjit: true
-  minimum_rust_version: ${RUST_VERSION}
-
-rust:
-  user: isucon
-  version: ${RUST_VERSION}
-  rustup_install_option: "--profile minimal"
 YAML
 
 sudo ${BUNDLE} exec itamae local --node-yaml $NODE_FILE test/cookbooks/default.rb
